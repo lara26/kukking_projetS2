306da6dbf2231f6b63337283c1d4b847a647a824
Martin <Martin@Martin-PC>
2015-05-21 17:56:32 +0200

    J'ai préféré faire une classe à coté pour la recherche ça évitera que ce soit trop le bazard
    La doc est en Français, je sais pas si Jean veux qu'on lui passe en
    Anglais (je peux m'en occuper dans ce cas là faut juste me le dire)
    
    J'ai pas vraiment trop compris la recherche dans les fichers Excel donc
    j'ai pas encore remplit sous le for mais à voir :)

---
+++ ProjectTestLectureJava/src/TestRecherche.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+
+public class TestRecherche 
+{
+	Scanner sc = new Scanner(System.in);
+	String recherche=sc.nextLine();
+	
+	
+	public void rechercheUtilisateur(String recherche)
+	{
+	while(recherche != Cell.titre)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+}
