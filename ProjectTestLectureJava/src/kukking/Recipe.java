961fbfa231ad1738b10b6da81a25d72564a987e7
RobinExtrant <RobinExtrant>
2015-05-24 19:41:54 +0200

    add methods on class Recipe to get differents elements

--- ProjectTestLectureJava/src/kukking/Recipe.java
+++ ProjectTestLectureJava/src/kukking/Recipe.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 import jxl.write.Label;
@@ -16,18 +17,155 @@ import jxl.write.biff.RowsExceededException;
 class Recipe {
 	public final static String sourcePath = "./receipts.xls";
 	private String nameRecipe;
+	
 
 	/**
-	 * renvoie tous les éléments de la recette dans un tableau 
+	 * @return recipe name
 	 */
-	public String obtenirElemsRecette() {
-	}
-
-
 	public String getNameRecipe() {
 		return nameRecipe;
 	}
+	/**
+	 * @return quantity of persons
+	 */
+	public int getNbPers() {
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			return Integer.parseInt(recipe.getCell("F3").getContents());
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return -1;
+	}
+	/**
+	 * @return preparation time
+	 */
+	public int getPreparationTime() {
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			return Integer.parseInt(recipe.getCell("F5").getContents());
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return -1;
+	}
 
+	/**
+	 * @return cooking time
+	 */
+	public int getCookingTime() {
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			return Integer.parseInt(recipe.getCell("F6").getContents());
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return -1;
+	}
+	
+	/**
+	 * @return recipe's cost
+	 */
+	public String getCost() {
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			return recipe.getCell("F6").getContents();
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return "";
+	}
+	
+	/**
+	 * @return categories list
+	 */
+	public ArrayList<String> getCategories() {
+		ArrayList <String> categories= new ArrayList<String>();
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			int numRow = 5;
+			while (recipe.getCell(3, numRow).getContents() != ""){
+				categories.add(recipe.getCell(3, numRow).getContents());
+				numRow ++;
+			}
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return categories;
+	}
+	
+	/**
+	 * @return ingredients list
+	 */
+	public ArrayList<String> getIngredients() {
+		ArrayList <String> ingredients= new ArrayList<String>();
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			int numRow = 5;
+			while (recipe.getCell(0, numRow).getContents() != ""){
+				ingredients.add(recipe.getCell(0, numRow).getContents());
+				numRow ++;
+			}
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return ingredients;
+	}
+	
+	/**
+	 * @return quantities list
+	 */
+	public ArrayList<String> getQuantities() {
+		ArrayList <String> quantities= new ArrayList<String>();
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			int numRow = 5;
+			while (recipe.getCell(1, numRow).getContents() != ""){
+				quantities.add(recipe.getCell(1, numRow).getContents());
+				numRow ++;
+			}
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return quantities;
+	}
+	
+	/**
+	 * @return units list
+	 */
+	public ArrayList<String> getUnits() {
+		ArrayList <String> units= new ArrayList<String>();
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			int numRow = 5;
+			while (recipe.getCell(2, numRow).getContents() != ""){
+				units.add(recipe.getCell(2, numRow).getContents());
+				numRow ++;
+			}
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return units;
+	}
+	
+	/**
+	 * @return preparation's steps
+	 */
+	public ArrayList<String> getPreparation() {
+		ArrayList <String> preparation= new ArrayList<String>();
+		try {
+			Workbook workbook = Workbook.getWorkbook(new File("./receipts.xls"));
+			Sheet recipe = workbook.getSheet(this.nameRecipe);
+			int numRow = 15;
+			while (recipe.getCell(4, numRow).getContents() != ""){
+				preparation.add(recipe.getCell(4, numRow).getContents());
+				numRow ++;
+			}
+		} catch (BiffException e) {e.printStackTrace();} catch (IOException e) {e.printStackTrace();} 
+		return preparation;
+	}
+	
+	
+	
+	/**
+	 * constructor to the application's launcher to create receipts from already existing receipts in file
+	 * @param nameRecipe
+	 */
+	public Recipe(String nameRecipe){
+		this.nameRecipe=nameRecipe;
+	}
 	/**
 	 * constructor when we create a new recipe
 	 * @param title
@@ -41,7 +179,7 @@ class Recipe {
 	 * @param units
 	 * @param preparation
 	 */
-	public Recipe(String title, int nbPers, int preparationTime, int cookingTime, String cost, ArrayList<String> categories, ArrayList<String> ingredients, ArrayList<String> quantities, ArrayList<String> units, ArrayList<String> preparation)
+ 	public Recipe(String title, int nbPers, int preparationTime, int cookingTime, String cost, ArrayList<String> categories, ArrayList<String> ingredients, ArrayList<String> quantities, ArrayList<String> units, ArrayList<String> preparation)
 	{
 		this.nameRecipe = title;
 		WritableWorkbook workbook = null;
